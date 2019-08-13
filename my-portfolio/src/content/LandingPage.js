a3f6e4f0c55fe07a40b7ad63297aa3c8a09cb3f3
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-12 19:38:36 -0700

    created cards for portfolio items

--- my-portfolio/src/content/LandingPage.js
+++ my-portfolio/src/content/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NavBar from '../components/NavBar';
 import AboutMe from '../components/AboutMe/AboutMe';
+import Projects from '../components/Projects/Projects';
 
 class LandingPage extends Component {
 
@@ -8,7 +9,10 @@ class LandingPage extends Component {
     return ( 
       <div>
         <NavBar />
-        <AboutMe className="landing-page--about-me"></AboutMe>/>
+        <AboutMe className="landing-page--about-me"></AboutMe>
+        <Projects />
+        <Projects />
+        <Projects />
       </div>
     );
   }
