2d4c34c841aa5f809c261e8cdab7ef6c6110364f
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-12 19:29:22 -0700

    Add Nav Bar

---
+++ my-portfolio/src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => (
+  <div className="navbar">
+    <a href="#home">About Me</a>
+    <a href="#news">Projects</a>
+    <a href="#contact">Contact</a>
+  </div>
+);
+
+export default NavBar;
--- my-portfolio/src/content/LandingPage.js
+++ my-portfolio/src/content/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavBar from '../components/NavBar';
 import AboutMe from '../components/AboutMe/AboutMe';
 
 class LandingPage extends Component {
@@ -6,6 +7,7 @@ class LandingPage extends Component {
   render () {
     return ( 
       <div>
+        <NavBar />
         <AboutMe className="landing-page--about-me"></AboutMe>/>
       </div>
     );
