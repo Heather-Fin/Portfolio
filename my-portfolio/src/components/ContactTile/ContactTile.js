5e6c0338d00785b54427129354b0e745c8a362d5
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-12 23:07:21 -0700

    formatted resource cards

--- my-portfolio/src/components/ContactTile/ContactTile.js
+++ my-portfolio/src/components/ContactTile/ContactTile.js
@@ -4,6 +4,7 @@ import PlaceHolderImg from '../../assets/placeholder-square.jpg';
 const ContactTile = () => (
   <div className="contact-tile">
     <img src={PlaceHolderImg} alt="" className="conact-tile--img" />
+    <h3 className="contact-tile--h3">Resource</h3>
   </div>
 );
 
