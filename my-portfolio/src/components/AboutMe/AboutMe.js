2eacdb7dbf0ecd3fdd43c0c20d0870d6b478e25b
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-13 09:26:43 -0700

    Changed color scheme

--- my-portfolio/src/components/AboutMe/AboutMe.js
+++ my-portfolio/src/components/AboutMe/AboutMe.js
@@ -5,10 +5,7 @@ const AboutMe = () => (
 
   <div className="about-me">
     <div className="about-me--content">
-      <h1 className="about-me--h1">Heather Finnegan</h1>
-      <p className="about-me--p">Heather Finnegan is a Computer Science graduate student studying at The University of Texas at Dallas. Her area of focus is in interactive computing and she is interested in Front End Development. 
-      <br /><br />She is a highly organized and team oriented individual who has a passion for creating content that improves people’s lives. In her free time, she loves learning about various topics including iOS App Development, Web Programming, and Virtual Reality. She is an enthusiast of podcasts, video games, hiking, and her dog!
-      </p>
+      <h1 className="about-me--h1">Hello, I'm <span className="about-me--h1-alt">Heather Finnegan</span>.<br />I'm a front-end web developer.</h1>
     </div>
     <div className="about-me--img">
       <img src={PortfolioImg} alt="Portrait of Heather Finnegan" className="about-me--portfolio-img" />
