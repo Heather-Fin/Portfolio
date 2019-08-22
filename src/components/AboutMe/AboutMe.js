bf03e3787f9fae3152a8d721d00f6a3df7dd3025
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-22 11:15:36 -0500

    changed about my text

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,9 +3,9 @@ import React from 'react';
 const AboutMe = () => (
   <div className="about-me">
     <p className="about-me--p">
-      Heather Finnegan is a Computer Science graduate student studying at The University of Texas at Dallas. Her area of focus is in interactive computing and she is interested in Front End Development.
+      I’m Heather, a Computer Science graduate student studying at The University of Texas at Dallas. My focus is in interactive computing and I am interested in Front End Development. Since my undergraduate degree was in Radio Television Film with a focus on VFX/Animation, Front End Development allows me to merge both my logical side with my creative skills.
       <br /><br />
-      She is a highly organized and team oriented individual who has a passion for creating content that improves people’s lives. In her free time, she loves learning about various topics including iOS App Development, Web Programming, and Virtual Reality. She is an enthusiast of podcasts, video games, hiking, and her dog!
+      I am a highly organized and team oriented individual who has a passion for creating content that will improve people’s lives. In my free time, I love learning about various topics including Virtual Reality, Web Programming, and App Development. I am an enthusiast of podcasts, video games, running, and my dog!
     </p>
   </div>
 );
