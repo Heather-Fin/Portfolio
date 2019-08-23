16b5e463a633448b001c4c8c104b0df236a03c55
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-23 11:38:52 -0500

    Added anchors to project images and titles

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,12 +5,18 @@ class Projects extends Component {
   render() {
     return (
       <div className="projects--card">
-          <img src={this.props.img} alt="Screengrab of project" className="projects--img" />
+          <a href={this.props.link} target="_blank">
+            <img src={this.props.img} alt="Screengrab of project" className="projects--img" />
+          </a>
           <div className="projects--col">
-            <h2 className="projects--h2">{this.props.title}</h2> 
-            <a href={this.props.link} target="_blank">
-              <img src={this.props.icon} alt="logo" className="projects--icon" />
-            </a>
+            <div className="projects--click-link">
+              <a href={this.props.link} target="_blank">
+                <h2 className="projects--h2">{this.props.title}</h2> 
+              </a>
+              <a href={this.props.link} target="_blank">
+                <img src={this.props.icon} alt="logo" className="projects--icon" />
+              </a>
+            </div>
             <h3 className="projects--h3">{this.props.date}</h3>
             <p className="projects--p">{this.props.description}</p>
           </div>
